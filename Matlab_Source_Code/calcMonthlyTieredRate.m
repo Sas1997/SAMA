1451f9036546c1baeb1e3320f8e4da8a38da99d8
Seyyed Ali Sadat <84838524+Sas1997@users.noreply.github.com>
2023-01-17 02:31:50 +0330

    Add files via upload

---
+++ Matlab_Source_Code/calcMonthlyTieredRate.m
@@ -0,0 +1,36 @@
+function [Cbuy,Base_charge]= calcMonthlyTieredRate(Base_charge_tier_1,Base_charge_tier_2,Base_charge_tier_3,prices, tierMax, load)
+    Cbuy = zeros(1,8760);
+    Base_charge = zeros(1,12);
+    daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    hCount = 1;
+    for m=1:12
+        monthlyLoad = 0;
+        for h=1:(24 * daysInMonth(m))
+            monthlyLoad= monthlyLoad + load(hCount);
+        hCount = hCount + 1;
+        end
+        totalmonthlyload(m,1)=monthlyLoad;
+    end
+        
+    hCount = 1;
+    for m=1:12
+        for h=1:(24 * daysInMonth(m))
+            if totalmonthlyload(m,1) < tierMax(m,1)
+                Cbuy(hCount) = prices(m,1);
+            elseif totalmonthlyload(m,1) < tierMax(m,2)
+                Cbuy(hCount) = prices(m,2);
+            else
+                Cbuy(hCount) = prices(m,3);
+            end
+                   hCount = hCount + 1; 
+        end
+
+         if totalmonthlyload(m,1) < tierMax(m,1)
+             Base_charge(m) = Base_charge_tier_1;
+         elseif totalmonthlyload(m,1) < tierMax(m,2)
+             Base_charge(m) = Base_charge_tier_2;
+         else 
+             Base_charge(m) = Base_charge_tier_3;
+         end 
+    end
+end
